a188c4ae28067832990cc0c024a347f94a6aaacd
giuseppegargani <sentierodisole1@yahoo.it>
2022-05-21 02:58:37 +0200

    aggiunta

--- app/src/test/java/com/example/jsonreport/ExampleUnitTest.kt
+++ app/src/test/java/com/example/jsonreport/ExampleUnitTest.kt
@@ -5,6 +5,7 @@ import com.example.instrumentedreport.GlobalUTClass
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Ignore
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -15,6 +16,15 @@ class ExampleUnitTest: GlobalUTClass() {
 
     @Test
     fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+    @Test
+    @Ignore
+    fun addition_isCorrect2() {
+        assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun addition_isCorrect3() {
         assertEquals(4, 2 + 2)
     }
 }
