1e8c48c5f8a3c32b07c941b4e0b5500755134f28
giuseppegargani <sentierodisole1@yahoo.it>
2022-06-21 13:58:49 +0200

    aggiunto SerializedName

--- instrumentedreport/src/main/java/com/example/instrumentedreport/CompositeTestClass.kt
+++ instrumentedreport/src/main/java/com/example/instrumentedreport/CompositeTestClass.kt
@@ -1,15 +1,23 @@
 package com.example.instrumentedreport
 
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 //I need to add the documentation for every class
+
+const val eccomi = "campo"
 
 data class CompositeTestClass (var package_name:String, var test_classes_list: MutableList<SingleClass>)
 
-data class SingleClass(var test_class_name:String,  var tests_list: MutableList<SingleTest>)
+data class SingleClass( var test_class_name:String,  var tests_list: MutableList<SingleTest>)
 
-data class SingleTest ( var testName: String, var outcome: String, var otherData: String? = null )
+@Serializable
+data class SingleTest (@SerializedName(eccomi) var testName: String, @SerializedName("Risultato") var outcome: String, var otherData: String? = null )
 
 enum class TestResultStatus {
     SUCCESS, ABORTED, FAILURE, SKIPPED
 }
 
-
+@Serializable
+data class MyClass(@SerialName(eccomi) val s1: Int)
